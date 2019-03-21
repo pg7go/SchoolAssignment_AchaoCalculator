aaf0e83cf925e1510aae531f2d10768d2276cd23
pg7go <13982719614@163.com>
2019-03-21 20:30:55 +0800

    第一次修改

---
+++ AchaoCalculator/AchaoCalculator/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AchaoCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "123",
+            "555"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("234");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("555");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("666");
+            this.metroUserControl1 = new MetroFramework.Controls.MetroUserControl();
+            this.metroListView1 = new MetroFramework.Controls.MetroListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // metroUserControl1
+            // 
+            this.metroUserControl1.Location = new System.Drawing.Point(550, 268);
+            this.metroUserControl1.Name = "metroUserControl1";
+            this.metroUserControl1.Size = new System.Drawing.Size(150, 150);
+            this.metroUserControl1.TabIndex = 2;
+            this.metroUserControl1.UseSelectable = true;
+            // 
+            // metroListView1
+            // 
+            this.metroListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.metroListView1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView1.FullRowSelect = true;
+            this.metroListView1.GridLines = true;
+            this.metroListView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.metroListView1.Location = new System.Drawing.Point(58, 75);
+            this.metroListView1.Name = "metroListView1";
+            this.metroListView1.OwnerDraw = true;
+            this.metroListView1.Size = new System.Drawing.Size(510, 248);
+            this.metroListView1.TabIndex = 3;
+            this.metroListView1.UseCompatibleStateImageBehavior = false;
+            this.metroListView1.UseSelectable = true;
+            this.metroListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "生成的四则运算";
+            this.columnHeader1.Width = 294;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "答案";
+            this.columnHeader2.Width = 212;
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(482, 2);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(58, 360);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.PromptText = "请输入生成数目后点击蓝色按钮";
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.ShowButton = true;
+            this.metroTextBox1.ShowClearButton = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(510, 30);
+            this.metroTextBox1.TabIndex = 4;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMark = "请输入生成数目后点击蓝色按钮";
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.ButtonClick += new MetroFramework.Controls.MetroTextBox.ButClick(this.metroTextBox1_ButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 424);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroListView1);
+            this.Controls.Add(this.metroUserControl1);
+            this.Name = "Form1";
+            this.Text = "阿超的四则运算生成器";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroUserControl metroUserControl1;
+        private MetroFramework.Controls.MetroListView metroListView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+    }
+}
+
---
+++ AchaoCalculator/AchaoCalculator/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AchaoCalculator
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroListView1.Items.Clear();
+        }
+
+        Random random = new Random();
+        private void CreateQuestion()
+        {
+            int num1 = random.Next() % 1000;
+            int num2 = random.Next() % 1000;
+            int cal_type = random.Next() % 4;
+            float result=0;
+            string fx="";
+            switch(cal_type)
+            {
+                case 0:
+                    fx = "+";
+                    result = num1 + num2;
+                    break;
+                case 1:
+                    fx = "-";
+                    result = num1 - num2;
+                    break;
+                case 2:
+                    fx = "×";
+                    result = num1 * num2;
+                    break;
+                case 3:
+                    fx = "÷";
+                    result = (float)num1 / num2;
+                    result = (float)Math.Round(result, 2);
+                    break;
+            }
+            ListViewItem item = new ListViewItem();
+            item.Text = num1.ToString() + " " + fx + " " + num2.ToString()+" = ";
+            item.SubItems.Add(result.ToString());
+            metroListView1.Items.Add(item);
+        }
+
+
+        private void metroTextBox1_ButtonClick(object sender, EventArgs e)
+        {
+            string text = metroTextBox1.Text;
+            if(int.TryParse(text,out int num))
+            {
+                metroListView1.Items.Clear();
+                for (int i = 0; i < num; i++)
+                {
+                    CreateQuestion();
+                }
+            }
+            else
+            {
+                MetroFramework.MetroMessageBox.Show(this,"请输入正确的数字！","错误提示：",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
